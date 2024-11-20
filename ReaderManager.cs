aa5d2346b439ebfa8f642cdd58d27244119b151d
AA-NoV <166708780+AA-NoV@users.noreply.github.com>
2024-11-20 09:59:34 +0300

    Add files via upload

---
+++ ReaderManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ReaderManager
+{
+    private List<Reader> Readers = new List<Reader>();
+    public bool AddReader(Reader reader)
+    {
+        if (Readers.Exists(r => r.Id == reader.Id))
+        {
+            return false;
+        }
+        Readers.Add(reader);
+        return true;
+    }
+    public void RemoveReader(string readerId)
+    {
+        Readers.RemoveAll(r => r.Id == readerId);
+    }
+    public Reader GetReader(string readerId)
+    {
+        return Readers.Find(r => r.Id == readerId);
+    }
+    public List<Reader> GetAllReaders()
+    {
+        return Readers;
+    }
+}
